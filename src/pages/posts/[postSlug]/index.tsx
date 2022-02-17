3c7978a1193c271ea743c61fa94c194196fb12c3
theodesp <theo.despoudis@wpengine.com>
2022-02-17 16:00:48 +0000

    feat: add posts and previews

---
+++ src/pages/posts/[postSlug]/index.tsx
@@ -0,0 +1,51 @@
+import { getNextStaticProps, is404 } from '@faustjs/next';
+import { client, Post } from 'client';
+import { GetStaticPropsContext } from 'next';
+import Head from 'next/head';
+
+export interface PostProps {
+  post: Post | Post['preview']['node'] | null | undefined;
+}
+
+export function PostComponent({ post }: PostProps) {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+  return (
+    <>
+      <Head>
+        <title>
+          {post?.title()} - {generalSettings?.title}
+        </title>
+      </Head>
+
+      <main className="content content-single">
+        <div className="wrap">
+          <div dangerouslySetInnerHTML={{ __html: post?.content() ?? '' }} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default function Page() {
+  const { usePost } = client;
+  const post = usePost();
+
+  return <PostComponent post={post} />;
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+    notFound: await is404(context, { client }),
+  });
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+}
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,29 @@
+import { getNextStaticProps } from '@faustjs/next';
+import { client,  } from 'client';
+import { GetStaticPropsContext } from 'next';
+import Head from 'next/head';
+import React from 'react';
+
+export default function Page() {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+  return (
+    <>
+      <Head>
+        <title>
+          {generalSettings?.title} - {generalSettings?.description}
+        </title>
+      </Head>
+
+      <main className="content content-index"/>
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+  });
+}
