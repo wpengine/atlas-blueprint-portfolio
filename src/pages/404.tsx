44f063db7e895cff9bbca37b556242676102b819
theodesp <theo.despoudis@wpengine.com>
2022-02-17 15:07:23 +0000

    feat(404.tsx): add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { client } from 'client';
+import Head from "next/head";
+
+export default function Page(): JSX.Element {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+  return (
+    <>
+      <Head>
+        <title>
+          {generalSettings?.title} - {generalSettings?.description}
+        </title>
+      </Head>
+      <main>
+        404 Page
+      </main>
+    </>
+  );
+}
