132b04d18403a24d938b381206afc909c0929c2e
Joe Fusco <joe.fusco@wpengine.com>
2022-03-14 17:04:19 -0400

    Add inline docs

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,6 +22,11 @@ function Projects({ projects, id, emptyText = 'No projects found.' }) {
               />
               <div className={styles['list-item-content']}>
                 <Heading level="h3">
+                  {/*
+                    Hardcoded CPT slug to be removed once ACM supports the
+                    `with_front` register_post_type() option.
+                    https://github.com/wpengine/atlas-content-modeler/discussions/457
+                  */}
                   <Link href={`projects/${project.slug}` ?? '#'}>
                     <a>{project.title()}</a>
                   </Link>
