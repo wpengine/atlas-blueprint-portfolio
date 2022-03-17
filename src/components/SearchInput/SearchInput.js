98254feb6b3084266e26d4ada59ab018204e3ded
Blake Wilson <blake.wilson@wpengine.com>
2022-03-17 14:09:48 -0500

    Auto focus search input

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,7 +1,15 @@
+import { useEffect, useRef } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import styles from './SearchInput.module.scss';
 
 export default function SearchInput({ value, onChange }) {
+  const input = useRef();
+
+  useEffect(() => {
+    input.current.value = '';
+    input.current.focus();
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <FaSearch className={styles.icon} />
@@ -10,12 +18,16 @@ export default function SearchInput({ value, onChange }) {
       </label>
 
       <input
+        ref={input}
         id="search"
+        name="search"
         className={styles.input}
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        autoFocus
+        onFocus={(e) => e.target.select()}
         type="text"
-        placeholder="Search..."
+        placeholder="Start typing..."
       />
     </div>
   );
