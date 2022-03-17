5cbabea5d83ab599863613dabc4e8be08267aee1
Blake Wilson <blake.wilson@wpengine.com>
2022-03-17 11:36:05 -0500

    Input a11y

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -5,7 +5,12 @@ export default function SearchInput({ value, onChange }) {
   return (
     <div className={styles.wrapper}>
       <FaSearch className={styles.icon} />
+      <label className="sr-only" htmlFor="search">
+        Search
+      </label>
+
       <input
+        id="search"
         className={styles.input}
         value={value}
         onChange={(e) => onChange(e.target.value)}
