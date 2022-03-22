1a2ac0dd0232e6e4b085a59e96eff655dfacba39
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2022-03-22 09:34:26 +0000

    Update src/components/TaxonomyTerms/TaxonomyTerms.js
    Co-authored-by: Blake Wilson <blake.wilson@wpengine.com>

--- src/components/TaxonomyTerms/TaxonomyTerms.js
+++ src/components/TaxonomyTerms/TaxonomyTerms.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styles from './TaxonomyTerms.module.scss';
 
 export default function TaxonomyTerms({ post, taxonomy }) {
-  const termLinks = post?.[taxonomy]()?.edges.map((edge) => {
+  const termLinks = post?.[taxonomy]?.()?.edges.map((edge) => {
     const { name, uri } = edge?.node;
     return uri && <Link href={uri}>{name}</Link>;
   });
