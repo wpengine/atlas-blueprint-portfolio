533d4346b16be3329867c1279089a05ab10fab14
Joe Fusco <joe.fusco@wpengine.com>
2022-03-23 10:20:41 -0400

    Format changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -74,7 +74,11 @@ export default function Header({ title, image, date, author }) {
       {image && (
         <div className={styles['image']}>
           <div className="container">
-            <FeaturedImage className={styles['featured-image']} image={image} priority />
+            <FeaturedImage
+              className={styles['featured-image']}
+              image={image}
+              priority
+            />
           </div>
         </div>
       )}
