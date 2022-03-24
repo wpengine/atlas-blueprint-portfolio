a8f6bc2f9a67605ec7323e4819b73e43ae75421f
Andrew Matthews <andrew.matthews@wpengine.com>
2022-03-24 14:01:31 -0400

    make default image high resolution (#54)

--- src/components/FeaturedImage/DefaultImage.js
+++ src/components/FeaturedImage/DefaultImage.js
@@ -2,17 +2,18 @@ import Image from 'next/image';
 
 import styles from './FeaturedImage.module.scss';
 
-function DefaultImage({ className }) {
-  const src = '/static/placeholder.png';
+function DefaultImage({ className, ...props }) {
+  const src = '/static/banner.jpeg';
+  const alt = 'Downtown Austin Skyline';
+
   return (
     <figure className={[styles['featured-image'], className].join(' ')}>
       <Image
         src={src}
-        width={3400}
-        height={3400}
-        alt="Placeholder"
+        alt={alt}
         layout="responsive"
-        objectFit="contain"
+        objectFit="cover"
+        {...props}
       />
     </figure>
   );
