55c28b71baa9d57c441ae89dd138eddab70c904f
Joe Fusco <joe.fusco@wpengine.com>
2022-03-24 11:34:48 -0400

    Update other import name

--- src/components/SkipNavigationLink/SkipNavigationLink.js
+++ src/components/SkipNavigationLink/SkipNavigationLink.js
@@ -1,4 +1,4 @@
-import * as SELECTOR from 'constants/selectors';
+import * as SELECTORS from 'constants/selectors';
 
 import styles from './SkipNavigationLink.module.scss';
 
@@ -6,7 +6,7 @@ export default function SkipNavigationLink() {
   return (
     <a
       className={[styles['skip-nav-link'], 'sr-only'].join(' ')}
-      href={`#${SELECTOR.MAIN_CONTENT_ID}`}
+      href={`#${SELECTORS.MAIN_CONTENT_ID}`}
     >
       Skip To Main Content
     </a>
