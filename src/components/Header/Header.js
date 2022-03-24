9db30ac1bdfd03876a322e33251e47e2a5d77ac6
Joe Fusco <joe.fusco@wpengine.com>
2022-03-24 17:32:53 -0400

    Import order

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
+import * as MENUS from 'constants/menus';
+
 import { useState } from 'react';
 import { FaBars, FaSearch } from 'react-icons/fa';
 import Image from 'next/image';
 import Link from 'next/link';
-
 import { NavigationMenu, SkipNavigationLink } from 'components';
-import * as MENUS from 'constants/menus';
 
 import styles from './Header.module.scss';
 
