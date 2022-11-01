e0ac0d44f009f50273700fc120aef6bccd6a601a
Blake Wilson <blake@blake.id>
2022-11-01 14:55:57 -0500

    Docs for relay plugin

--- plugins/RelayStylePaginationPlugin.js
+++ plugins/RelayStylePaginationPlugin.js
@@ -1,5 +1,11 @@
 import { relayStylePagination } from '@apollo/client/utilities';
 
+/**
+ * This plugin adds Relay-style cursor pagination to the Faust/Apollo client
+ * via the plugin system for posts/contentNodes
+ *
+ * @link https://www.apollographql.com/docs/react/pagination/cursor-based/#relay-style-cursor-pagination
+ */
 export class RelayStylePaginationPlugin {
   apply({ addFilter }) {
     addFilter('apolloClientInMemoryCacheOptions', 'faust', (options) => {
