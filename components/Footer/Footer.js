b15adb66ad6bec54b8d76388fde303594ec995c3
Theofanis Despoudis <328805+theodesp@users.noreply.github.com>
2024-10-01 12:40:33 +0100

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -120,7 +120,9 @@ export default function Footer({ menuItems }) {
 
         <div className={cx('copyright')}>
           &copy; {new Date().getFullYear()} Blueprint Media &#183; Powered By{' '}
-          <a href="https://wpengine.com/headless-wordpress">Headless Platform</a>
+          <a href="https://wpengine.com/headless-wordpress">
+            Headless Platform
+          </a>
         </div>
       </div>
     </footer>
